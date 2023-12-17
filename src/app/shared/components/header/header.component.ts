1db90bc9640cdc8de11229ee679865c8928e113e
Oscar <oscardelafuentetoledo10@gmail.com>
2023-12-17 19:11:28 +0100

    creados botones para dark Mode

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent  implements OnInit {
 
+  @Input() title: string = '';
+  @Input() backButton: string = '';
+  @Input() isModal: boolean = false;
+  @Input() color: string = '';
+  @Input() centerTitle: boolean = false;
+
+  //GETS
+  /** Indicamos el color del toolbar header dependiendo si tiene o no valor el input */
+  get colorHeader(): string {
+    return this.color ? this.color : 'primary';
+  }
+
+  darkMode: boolean = false;
+
   constructor() { }
 
   ngOnInit() {}
+
+  setTheme(darkMode: boolean): void {
+    this.darkMode = darkMode;
+  }
 
 }
